d734ee9fb32f7e398d68b575c784e238d9cb559e
Sagar Chalise <sagar@desktop.(none)>
2010-10-04 23:59:52 +0545

    Buzhug Improvement

--- data.py
+++ data.py
@@ -7,17 +7,34 @@ from buzhug import Base
 class DataBase():
     def __init__(self):
         self.db=Base(mypath)
+        self.sfield = {'शब्द': 'Words', 
+                        'पद': 'Speech', 
+                        'अर्थ': 'Meaning', 
+                        'पर्यायवाचि': 'Synonym', 
+                        'विपरीतार्थक': 'Antonym', 
+                        'अंग्रेजी': 'English'}
         self.db.open()
             	
+    def _get_attribute(self,args):
+        ret_val = {}
+        for k,f in self.sfield.items():
+            ret_val[k] = getattr(args, f)
+            if ret_val[k] == None:
+                ret_val.pop(k)
+        return ret_val
     def select(self,value):
-        record =self.db.select(['Words','Speech','Meaning','Synonym','Antonym','English'],Words=value)
-        for v in record:
-            return v
-        self.db.close()
+        self.record = self.db.select([f for f in self.sfield.itervalues()],Words=value)
+        for v in self.record:
+            self.g = self._get_attribute(v)
+            return self.g             
+        self.db.close()             
+    
     def fields(self):
         print self.db.field_names                   		
-mydb=DataBase()
+	
+    def get_field_length(self):
+        return len(self.g)
 
-if __name__=='__main__':
-	print "Hello, Not Allowed."
+mydb=DataBase()
+if __name__=='__main__':print "Hello, Not Allowed."
 
